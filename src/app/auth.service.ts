fa8bdde52d0992cd122ee9418d74cafe776a43cf
sajadkhosravi <sajadkhosravi1996@gmail.com>
2018-06-23 16:21:48 +0430

    login logic complete

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {isBoolean} from 'util';
 
 @Injectable()
 export class AuthService {
 
-  private login_url = 'http://127.0.0.1:8000/sign_in'
+  private login_url = 'http://127.0.0.1:8000/api/sign_in'
 
   constructor(private http: HttpClient) { }
 
   public login(name , password) {
-    this.http.post(this.login_url, '{name: name, password: password}')
+    this.http.post(this.login_url, {name: name , password: password })
       .subscribe(
         res => {
-          alert(res);
+          if (res['result'] === 'true') {
+            alert(1);
+          }
         },
         err => {
-          console.log('Error occured');
+          console.log(err);
         }
       );
   }
